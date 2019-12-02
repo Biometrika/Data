c8c510a84d1ffd3da87d17736256754e8cdbfb9f
Biometrika <crop.biometrics@gmail.com>
2019-12-02 21:57:55 +0100

    Add files via upload

--- korelacija-tackasti dijagram.R
+++ korelacija-tackasti dijagram.R
@@ -6,12 +6,12 @@ corrdata <- function(samples = 200, r = 0){
   data <- mvrnorm(n = samples, mu = c(0, 0), Sigma = matrix(c(1,r,r,1), nrow = 2), empirical = TRUE)
   X <- data[,1]
   Y <- data[,2]
-  data.frame(x = X, y =Y)
+  data.frame(x = X, y = Y)
 }
 
-df <-data.frame()
+df <- data.frame()
 for (i in c(1,0.8,0.5,0.2,0.0,-0.2,-0.5,-0.8,-1.0)){
-  tmp = corrdata(200,i)
+  tmp = corrdata(200, i)
   tmp["corr"] = i
   df <- rbind(df, tmp)
 }
